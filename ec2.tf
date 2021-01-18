df7af6c674838ac2ae9e910b942017fa4b66b0f6
vivekdevopshub <36101003+vivekdevopshub@users.noreply.github.com>
2021-01-18 14:13:21 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "MediaWiki-Server" {
   ami                    = "ami-04b1ddd35fd71475a"
   instance_type          = "t2.micro"
-  key_name               = "MediaWiki_key"           // Key can be managed separately but as of now key is assigned from console.
+  key_name               = "aws_key"           // Key can be managed separately but as of now key is assigned from console.
   vpc_security_group_ids = ["${aws_security_group.mediawiki-sg.id}"]
 
    tags = {
